d94f2dc8b44e5d50feea25fd934fc824a1097e4f
GfZ02 <farias._02f@outlook.com>
2021-07-13 08:10:32 -0300

    Novo projeto

---
+++ fuçando/Core/colors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace fuçando.Core
+{
+    class colors
+    {
+
+        public void Cores()
+        {
+
+        }
+
+        public SolidColorBrush getcoroff()
+        {
+            Color color = new Color();
+            color = Color.FromRgb(69, 70, 240);
+            SolidColorBrush coro = new SolidColorBrush(color);
+
+            return coro;
+        }
+
+        public SolidColorBrush getcoron()
+        {
+            Color color = new Color();
+            color = Color.FromRgb(52, 123, 217);
+            SolidColorBrush coro = new SolidColorBrush(color);
+
+            return coro;
+        }
+
+        public SolidColorBrush getcorlb()
+        {
+            Color color = new Color();
+            color = Color.FromRgb(227, 45, 250);
+            SolidColorBrush coro = new SolidColorBrush(color);
+
+            return coro;
+        }
+
+    }
+
+}
---
+++ fuçando/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace fuçando
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+
+        Core.colors cor = new Core.colors();
+
+        Page pag = new Page();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            bt1.Foreground = cor.getcorlb();
+            bt1.Background = cor.getcoron();
+            bt2.Background = cor.getcoroff();
+            bt3.Background = cor.getcoroff();
+
+            bt1.Padding = new Thickness(40, 0, 0, 0);
+            bt2.Padding = new Thickness(20, 0, 0, 0);
+            bt3.Padding = new Thickness(20, 0, 0, 0);
+
+
+            pnprinc.Content = new automa();
+
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            bt1.Foreground = cor.getcorlb();
+            bt2.Foreground = Brushes.Black;
+            bt3.Foreground = Brushes.Black;
+
+            bt1.Background = cor.getcoron();
+            bt2.Background = cor.getcoroff();
+            bt3.Background = cor.getcoroff();
+
+            bt1.Padding = new Thickness(40, 0, 0, 0);
+            bt2.Padding = new Thickness(20, 0, 0, 0);
+            bt3.Padding = new Thickness(20, 0, 0, 0);
+
+            pnprinc.Content = new automa();
+
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            bt1.Foreground = Brushes.Black;
+            bt3.Foreground = Brushes.Black;
+            bt2.Foreground = cor.getcorlb();
+
+            bt1.Background = cor.getcoroff();
+            bt2.Background = cor.getcoron();
+            bt3.Background = cor.getcoroff();
+
+            bt1.Padding = new Thickness(20, 0, 0, 0);
+            bt2.Padding = new Thickness(40, 0, 0, 0);
+            bt3.Padding = new Thickness(20, 0, 0, 0);
+
+            pnprinc.Content = new sup();
+
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            bt1.Foreground = Brushes.Black;
+            bt2.Foreground = Brushes.Black;
+            bt3.Foreground = cor.getcorlb();
+
+            bt1.Background = cor.getcoroff();
+            bt2.Background = cor.getcoroff();
+            bt3.Background = cor.getcoron();
+
+            bt1.Padding = new Thickness(20, 0, 0, 0);
+            bt2.Padding = new Thickness(20, 0, 0, 0);
+            bt3.Padding = new Thickness(40, 0, 0, 0);
+
+            pnprinc.Content = new relat();
+        }
+
+    }
+}
